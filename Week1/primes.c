30be65e9cbbb48c3b0dbc7f9d410095ad3ad33b1
Stephanie Shea <stephcshea@gmail.com>
2018-04-07 20:08:41 +0000

    added one solution to finding if number is  prime

---
+++ Week1/primes.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+int checkPrimeNumber(int n);
+
+int main()
+{
+  int n, flag;
+
+  printf("enter a positive integer: ");
+  scanf("%d", &n);
+
+  flag = checkPrimeNumber(n);
+
+  if (flag == 1)
+    printf("%d is not a prime number. \n", n);
+  else
+    printf("%d is a prime number. \n", n);
+  return 0;
+}
+
+int checkPrimeNumber(int n)
+{
+  int i;
+
+  for(i =2; i <= n/2; i++)
+  {
+    if(n%i == 0)
+      return 1;
+  }
+  return 0;
+}
